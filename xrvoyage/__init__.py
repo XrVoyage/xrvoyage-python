5d45c3bc88b156421589d0ccebb1145d8580c2b8
Peter Styk <peter@neux.io>
2024-05-23 15:44:52 +0100

    updated version gen

--- xrvoyage/__init__.py
+++ xrvoyage/__init__.py
@@ -1,8 +1,4 @@
 # xrvoyage/__init__.py
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "0.0.0"
+from .handlers.static import get_version, print_version
 
-def print_version():
-    print(__version__)
+__version__ = get_version()
