220c7e6f15c8061b7a8612026285bbd4219a5c61
Peter Styk <peter@styk.tv>
2024-07-19 07:08:18 +0100

    launching changes for the tests

--- control_agent_ve3ptv.py
+++
@@ -1,4 +0,0 @@
-from xroai.agents_pi.control_agent_ve3ptv import main
-from dotenv import load_dotenv
-load_dotenv()
-main()
---
+++ start_control_agent_ve3.py
@@ -0,0 +1,4 @@
+from xroai.agents_pi.control_agent_ve3 import main
+from dotenv import load_dotenv
+load_dotenv()
+main()
